15b30de4965c0ba72b79d04028de9f0524eda75e
Abeel Muhammad <abeelmuhammad50@gmail.com>
2020-04-30 10:00:45 +0500

    complted

---
+++ Unit Conversion/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Unit_Conversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputlabel = new System.Windows.Forms.Label();
+            this.outputlabel = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.inchToCmButton = new System.Windows.Forms.Button();
+            this.CmToInchButton = new System.Windows.Forms.Button();
+            this.FootToCmButton = new System.Windows.Forms.Button();
+            this.CmToFootButton = new System.Windows.Forms.Button();
+            this.YardToCmButton = new System.Windows.Forms.Button();
+            this.CmToYardButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputlabel
+            // 
+            this.inputlabel.AutoSize = true;
+            this.inputlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputlabel.Location = new System.Drawing.Point(24, 96);
+            this.inputlabel.Name = "inputlabel";
+            this.inputlabel.Size = new System.Drawing.Size(49, 22);
+            this.inputlabel.TabIndex = 0;
+            this.inputlabel.Text = "Input";
+            this.inputlabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // outputlabel
+            // 
+            this.outputlabel.AutoSize = true;
+            this.outputlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputlabel.Location = new System.Drawing.Point(475, 90);
+            this.outputlabel.Name = "outputlabel";
+            this.outputlabel.Size = new System.Drawing.Size(64, 22);
+            this.outputlabel.TabIndex = 5;
+            this.outputlabel.Text = "Output";
+            this.outputlabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(175, 90);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(264, 28);
+            this.inputTextBox.TabIndex = 6;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(635, 90);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(266, 28);
+            this.outputTextBox.TabIndex = 7;
+            // 
+            // inchToCmButton
+            // 
+            this.inchToCmButton.AutoSize = true;
+            this.inchToCmButton.Location = new System.Drawing.Point(28, 213);
+            this.inchToCmButton.Name = "inchToCmButton";
+            this.inchToCmButton.Size = new System.Drawing.Size(113, 35);
+            this.inchToCmButton.TabIndex = 8;
+            this.inchToCmButton.Text = "Inch to Cm";
+            this.inchToCmButton.UseVisualStyleBackColor = true;
+            this.inchToCmButton.Click += new System.EventHandler(this.inchToCmButton_Click);
+            // 
+            // CmToInchButton
+            // 
+            this.CmToInchButton.AutoSize = true;
+            this.CmToInchButton.Location = new System.Drawing.Point(175, 213);
+            this.CmToInchButton.Name = "CmToInchButton";
+            this.CmToInchButton.Size = new System.Drawing.Size(113, 35);
+            this.CmToInchButton.TabIndex = 9;
+            this.CmToInchButton.Text = "Cm to Inch";
+            this.CmToInchButton.UseVisualStyleBackColor = true;
+            this.CmToInchButton.Click += new System.EventHandler(this.CmToInchButton_Click);
+            // 
+            // FootToCmButton
+            // 
+            this.FootToCmButton.AutoSize = true;
+            this.FootToCmButton.Location = new System.Drawing.Point(326, 213);
+            this.FootToCmButton.Name = "FootToCmButton";
+            this.FootToCmButton.Size = new System.Drawing.Size(113, 35);
+            this.FootToCmButton.TabIndex = 10;
+            this.FootToCmButton.Text = "Foot to Cm";
+            this.FootToCmButton.UseVisualStyleBackColor = true;
+            this.FootToCmButton.Click += new System.EventHandler(this.FootToCmButton_Click);
+            // 
+            // CmToFootButton
+            // 
+            this.CmToFootButton.AutoSize = true;
+            this.CmToFootButton.Location = new System.Drawing.Point(479, 213);
+            this.CmToFootButton.Name = "CmToFootButton";
+            this.CmToFootButton.Size = new System.Drawing.Size(113, 35);
+            this.CmToFootButton.TabIndex = 11;
+            this.CmToFootButton.Text = "Cm to Foot";
+            this.CmToFootButton.UseVisualStyleBackColor = true;
+            this.CmToFootButton.Click += new System.EventHandler(this.CmToFootButton_Click);
+            // 
+            // YardToCmButton
+            // 
+            this.YardToCmButton.AutoSize = true;
+            this.YardToCmButton.Location = new System.Drawing.Point(635, 213);
+            this.YardToCmButton.Name = "YardToCmButton";
+            this.YardToCmButton.Size = new System.Drawing.Size(113, 35);
+            this.YardToCmButton.TabIndex = 12;
+            this.YardToCmButton.Text = "Yard to Cm";
+            this.YardToCmButton.UseVisualStyleBackColor = true;
+            this.YardToCmButton.Click += new System.EventHandler(this.YardToCmButton_Click);
+            // 
+            // CmToYardButton
+            // 
+            this.CmToYardButton.AutoSize = true;
+            this.CmToYardButton.Location = new System.Drawing.Point(788, 213);
+            this.CmToYardButton.Name = "CmToYardButton";
+            this.CmToYardButton.Size = new System.Drawing.Size(113, 35);
+            this.CmToYardButton.TabIndex = 13;
+            this.CmToYardButton.Text = "Cm to Yard";
+            this.CmToYardButton.UseVisualStyleBackColor = true;
+            this.CmToYardButton.Click += new System.EventHandler(this.CmToYardButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 278);
+            this.Controls.Add(this.CmToYardButton);
+            this.Controls.Add(this.YardToCmButton);
+            this.Controls.Add(this.CmToFootButton);
+            this.Controls.Add(this.FootToCmButton);
+            this.Controls.Add(this.CmToInchButton);
+            this.Controls.Add(this.inchToCmButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.outputlabel);
+            this.Controls.Add(this.inputlabel);
+            this.Name = "Form1";
+            this.Text = "Unit Conversion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label inputlabel;
+        private System.Windows.Forms.Label outputlabel;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button inchToCmButton;
+        private System.Windows.Forms.Button CmToInchButton;
+        private System.Windows.Forms.Button FootToCmButton;
+        private System.Windows.Forms.Button CmToFootButton;
+        private System.Windows.Forms.Button YardToCmButton;
+        private System.Windows.Forms.Button CmToYardButton;
+    }
+}
+
---
+++ Unit Conversion/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit_Conversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // converts Inches to Centimeters
+        public double Inch2Cm(double inch)
+        {
+            return inch * 2.54;
+        }
+
+        // converts Centimeters to Inches
+        public double Cm2Inch(double cm)
+        {
+            return cm / 2.54;    
+        }
+
+        // converts foots to Centimeters
+        public double Foot2Cm(double foot)
+        {
+            return foot * 30.48;
+        }
+
+        // converts Centimeters to Foots
+        public double Cm2Foot(double cm)
+        {
+            return cm / 30.48;
+        }
+
+        // Converts Yards to Centimeters
+        public double Yard2Cm(double yard)
+        {
+            return yard * 91.44;
+        }
+
+        // converts centimeters to yards
+        public double Cm2Yard(double cm)
+        {
+            return cm / 91.44;
+        }
+
+        // Input Text Box Validation
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(inputTextBox.Text, "[^0-9.]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                inputTextBox.Text = inputTextBox.Text.Remove(inputTextBox.Text.Length - 1);
+            }
+        }
+
+        // converts inches to centimeter
+        private void inchToCmButton_Click(object sender, EventArgs e)
+        {
+            if (this.inputTextBox.Text == "")
+            {
+                MessageBox.Show("Input Cannot be empty");
+                return;
+            }
+
+            inputlabel.Text = "Inches:";
+            outputlabel.Text = "Centimeters:";
+            outputTextBox.Text = String.Format("{0:0.00}", Inch2Cm(Double.Parse(inputTextBox.Text)));
+        }
+
+        // Converts Centimeters to Inches
+        private void CmToInchButton_Click(object sender, EventArgs e)
+        {
+            if (this.inputTextBox.Text == "")
+            {
+                MessageBox.Show("Input Cannot be empty");
+                return;
+            }
+
+            inputlabel.Text = "Centimeters:";
+            outputlabel.Text = "Inches:";
+            outputTextBox.Text = String.Format("{0:0.00}", Cm2Inch(Double.Parse(inputTextBox.Text)));
+        }
+
+        // Converts Foot to centimeters
+        private void FootToCmButton_Click(object sender, EventArgs e)
+        {
+            if (this.inputTextBox.Text == "")
+            {
+                MessageBox.Show("Input Cannot be empty");
+                return;
+            }
+
+            inputlabel.Text = "Foot:";
+            outputlabel.Text = "Centimeters:";
+            outputTextBox.Text = String.Format("{0:0.00}", Foot2Cm(Double.Parse(inputTextBox.Text)));
+        }
+
+        // converts Centimeters to Foot
+        private void CmToFootButton_Click(object sender, EventArgs e)
+        {
+            if (this.inputTextBox.Text == "")
+            {
+                MessageBox.Show("Input Cannot be empty");
+                return;
+            }
+
+            inputlabel.Text = "Centimeters:";
+            outputlabel.Text = "Foot:";
+            outputTextBox.Text = String.Format("{0:0.00}", Cm2Foot(Double.Parse(inputTextBox.Text)));
+        }
+
+        // Converts Yards to Centimeters
+        private void YardToCmButton_Click(object sender, EventArgs e)
+        {
+            if (this.inputTextBox.Text == "")
+            {
+                MessageBox.Show("Input Cannot be empty");
+                return;
+            }
+
+            inputlabel.Text = "Yards:";
+            outputlabel.Text = "Centimeters:";
+            outputTextBox.Text = String.Format("{0:0.00}", Yard2Cm(Double.Parse(inputTextBox.Text)));
+        }
+
+        // Converts Centimeters to Yards
+        private void CmToYardButton_Click(object sender, EventArgs e)
+        {
+            if (this.inputTextBox.Text == "")
+            {
+                MessageBox.Show("Input Cannot be empty");
+                return;
+            }
+
+            inputlabel.Text = "Centimeters:";
+            outputlabel.Text = "Yards:";
+            outputTextBox.Text = String.Format("{0:0.00}", Cm2Yard(Double.Parse(inputTextBox.Text)));
+        }
+    }
+}
